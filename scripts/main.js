3d1d83f16576240ee0b15e3c79fc3fc3a83f8465
Sanjay Kumar Pattanayak <38511110+spattana-s@users.noreply.github.com>
2018-11-03 16:37:14 -0400

    Delete main.js

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-var myHeading = document.querySelector('h1');
-//myHeading.textContent = 'Hello world!';
-//myHeading.textContent = 3 + 9 ;
-myHeading.textContent = 12/3;
-document.querySelector('html').onclick = function() {
-    alert('Ouch! Stop poking me!');
-}
